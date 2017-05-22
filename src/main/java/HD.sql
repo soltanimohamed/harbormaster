9839b6492084db5db942b9040f4ef937c68b878c
U-mohamed-Dator\mohamed <mohamed_soltani@live.com>
2017-05-22 10:28:19 +0200

    HD

--- src/main/java/HD.sql
+++ src/main/java/HD.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS employee(Employee_id integer primary key not null, FirstName text not null, LastName text not null, Gender_ID text not null, Driving_license_ID integer not null, Status_ID integer not null, Schedule_ID integer);
+CREATE TABLE IF NOT EXISTS employee(Employee_id integer primary key not null, FirstName text not null, LastName text not null, Gender text not null, Driving_license_ID integer not null, Status_ID integer not null, Schedule_ID integer);
 INSERT INTO "employee" (FirstName, LastName, Gender, Driving_license_ID, Status_ID, Schedule_ID) VALUES("Jonas","Lindberg", "Male", 1, 1, 1);
 
 CREATE TABLE IF NOT EXISTS employee_status(Status_ID integer primary key not null, Status text not null);
