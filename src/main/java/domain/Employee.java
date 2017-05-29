041f5c16e1438122ca683b316559000f1216550e
U-mohamed-Dator\mohamed <mohamed_soltani@live.com>
2017-05-29 11:29:32 +0200

    Employee class updated, shift attribute is added

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -7,8 +7,9 @@ private String gender; // Male,  Female
 private int driving_license_ID; //1: A, 2: AA, 3: B, 4: BB, 5: C, 6: CC, 7: CCC, 8: K
 private int status_ID; //1: 100%, 2: 50%, 3: Sjuk, 4: VAB, 5: Studier, 6: Semester
 private int schedule_ID; // 1: MF, 2: LS, 3: S
+private int shiftHours; // 1 : dag, 2: kväll, 3: natt
 
-public Employee(int employee_id, String firstName, String lastName, String gender, int driving_license_ID, int status_ID, int schedule_ID){
+public Employee(int employee_id, String firstName, String lastName, String gender, int driving_license_ID, int status_ID, int schedule_ID, int shiftHours){
   this.employee_id = employee_id;
   this.firstName = firstName;
   this.lastName = lastName;
@@ -16,6 +17,7 @@ public Employee(int employee_id, String firstName, String lastName, String gende
   this.driving_license_ID = driving_license_ID;
   this.status_ID = status_ID;
   this.schedule_ID = schedule_ID;
+  this.shiftHours = shiftHours;
 }
 
 public int employee_id(){ return employee_id;}
@@ -25,6 +27,7 @@ public String gender(){ return gender;}
 public int driving_license_ID(){ return driving_license_ID;}
 public int status_ID(){ return status_ID;}
 public int schedule_ID(){ return schedule_ID;}
+public int schiftHours(){ return shiftHours;}
 
 public void set_firstName(String firstName){this.firstName = firstName;}
 public void set_lastName(String lastName){this.lastName = lastName;}
@@ -32,7 +35,7 @@ public void set_gender(String gender){this.gender = gender;}
 public void set_driving_license_ID(int driving_license_ID){this.driving_license_ID = driving_license_ID;}
 public void set_status_ID(int status_ID){this.status_ID = status_ID;}
 public void set_schedule_ID(int schedule_ID){this.schedule_ID = schedule_ID;}
-
+public void set_shiftHours(int shiftHours){this.shiftHours = shiftHours;}
 @Override
 public boolean equals(Object o){
   if( o instanceof Employee){
@@ -74,9 +77,17 @@ public String license(){
       return em_schema;
       }
 
+      public String shift(){
+        String em_schift = "";
+        if(shiftHours == 1){em_schift = "Dag";}
+        else if(shiftHours == 2){em_schift ="Kväll";}
+        else if(shiftHours == 3){em_schift ="Natt";}
+        return em_schift;
+        }
+
     @Override
     public String toString(){
-      return employee_id + "," + firstName + "," + lastName + "," + gender +
-       "," + license() + "," + status() + "," + schema();
+      return employee_id + "| " + firstName + "| " + lastName + "| " + gender +
+       "| " + license() + "| " + status() + "| " + schema() + "| " + shift();
     }
 }
