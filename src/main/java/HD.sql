2e07a1cd90b75b5dcb3e59669bd5a2610d41c714
U-mohamed-Dator\mohamed <mohamed_soltani@live.com>
2017-05-29 10:46:41 +0200

    HD.sql updated

--- src/main/java/HD.sql
+++ src/main/java/HD.sql
@@ -1,6 +1,6 @@
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS employee(Employee_id integer primary key not null, FirstName text not null, LastName text not null, Gender text not null, Driving_license_ID integer not null, Status_ID integer not null, Schedule_ID integer);
+CREATE TABLE IF NOT EXISTS employee(Employee_id integer primary key not null, FirstName text not null, LastName text not null, Gender text not null, Driving_license_ID integer not null, Status_ID integer not null, Schedule_ID integer, ShiftHours integer not null);
 INSERT INTO "employee" (FirstName, LastName, Gender, Driving_license_ID, Status_ID, Schedule_ID) VALUES("Jonas","Lindberg", "Male", 1, 1, 1);
 
 CREATE TABLE IF NOT EXISTS employee_status(Status_ID integer primary key not null, Status text not null);
@@ -56,7 +56,7 @@ INSERT INTO "admin" (Admin_ID, username, password) VALUES(1, "mohamed", "abc");
 
 CREATE TABLE IF NOT EXISTS quay(Quay_ID integer primary key not null, Name text not null, QuayShift_ID integer not null); --Återkom och kolla tabeller för volymtyper
 
-CREATE TABLE IF NOT EXISTS quay_shift(QuayShift_ID integer primary key, Ship_id integer not null, ShiftHours text unique not null check (ShiftHours in('0:00-8:00', '8:00-16:00', '16:00-00:00')), ShiftDay text not null);
+CREATE TABLE IF NOT EXISTS quay_shift(QuayShift_ID integer primary key, Ship_id integer not null, ShiftHours integer unique not null, ShiftDay text not null);
 
 CREATE TABLE IF NOT EXISTS quays_volume_types(Volume_type text not null unique check(Volume_type in('A001','AA01','B001','BB01','C001','CC01','CCC1','K001')), Quay_ID integer not null check (Quay_ID in(1, 2, 3)));
 INSERT INTO quays_volume_types(Volume_type, Quay_ID) VALUES ('A001', 1);
